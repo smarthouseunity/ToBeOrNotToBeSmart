ce74188fcbf6206025aa52e50c76ca1f8ecd3bbc
chepaa <51419187+chepaa@users.noreply.github.com>
2019-06-05 21:55:40 +0300

    Sum
    C#

---
+++ New Unity Project/Assets/ColwithP.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColwithP : MonoBehaviour
+{
+    
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Parking Gate")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
